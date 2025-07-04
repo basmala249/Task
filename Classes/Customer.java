4c8de653a8b4b423beb10e0c2a75b18dff6b9d99
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-07-04 22:51:08 +0300

    Create Customer.java

---
+++ Classes/Customer.java
@@ -0,0 +1,64 @@
+package Classes;
+
+public class Customer {
+    private Integer nationalID ;
+    private String name;
+    private String email;
+    private String Address ;
+    private Integer phone;
+    private Integer Balance;
+    Customer(Integer ID , String name , String email , String Address , Integer phone , Integer Balance){
+        this.nationalID = ID;
+        this.name = name;
+        this.email = email;
+        this.Address = Address;
+        this.phone = phone;
+        this.Balance = Balance;
+    }
+    public Integer getNationalID() {
+        return nationalID;
+    }
+    public void setNationalID(Integer nationalID) {
+        this.nationalID = nationalID;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+    public Integer getPhone() {
+        return phone;
+    }
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+    public Integer getBalance() {
+        return Balance;
+    }
+    public void setBalance(Integer balance) {
+        Balance = balance;
+    }
+
+    public void updateBalance(Integer quantity , Integer op){
+         if((Balance + (quantity * op)) < 0){
+            System.err.println("You exceeded your balance");
+            return ;
+         }
+         Balance -= (quantity * op) ;
+    }
+
+    
+}
